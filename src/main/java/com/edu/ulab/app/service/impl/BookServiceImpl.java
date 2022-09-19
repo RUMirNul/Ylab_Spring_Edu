2ab99d3d44f37b58fb614df6f1da618524fa86e8
lexacv01@gmail.com <Agent3228>
2022-09-19 23:52:13 +0300

    Update BookService Update BookServiceImpl Update UserServiceImpl

--- src/main/java/com/edu/ulab/app/service/impl/BookServiceImpl.java
+++ src/main/java/com/edu/ulab/app/service/impl/BookServiceImpl.java
@@ -1,31 +1,49 @@
 package com.edu.ulab.app.service.impl;
 
+import com.edu.ulab.app.dao.BookDao;
 import com.edu.ulab.app.dto.BookDto;
+import com.edu.ulab.app.entity.BookEntity;
+import com.edu.ulab.app.mapper.BookMapper;
 import com.edu.ulab.app.service.BookService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class BookServiceImpl implements BookService {
+
+    private final BookDao<BookEntity, Long> bookDao;
+    private final BookMapper bookMapper;
+
+    public BookServiceImpl(BookDao<BookEntity, Long> bookDao, BookMapper bookMapper) {
+        this.bookDao = bookDao;
+        this.bookMapper = bookMapper;
+    }
+
     @Override
     public BookDto createBook(BookDto bookDto) {
-        bookDto.setId(22L);
-        return bookDto;
+        return bookMapper.bookEntityToBookDto(bookDao.createBook(bookMapper.bookDtoToBookEntity(bookDto)));
     }
 
     @Override
     public BookDto updateBook(BookDto bookDto) {
-        return null;
+        return bookMapper.bookEntityToBookDto(bookDao.updateBook(bookMapper.bookDtoToBookEntity(bookDto)));
     }
 
     @Override
     public BookDto getBookById(Long id) {
-        return null;
+        return bookMapper.bookEntityToBookDto(bookDao.getBookById(id));
     }
 
     @Override
     public void deleteBookById(Long id) {
+        bookDao.deleteBookById(id);
+    }
 
+    @Override
+    public List<Long> getAllBookIdByUserId(Long id) {
+        return bookDao.getAllBookIdByUserId(id);
     }
 }
--- src/main/java/com/edu/ulab/app/service/impl/UserServiceImpl.java
+++ src/main/java/com/edu/ulab/app/service/impl/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.edu.ulab.app.service.impl;
 
+import com.edu.ulab.app.dao.UserDao;
 import com.edu.ulab.app.dto.UserDto;
+import com.edu.ulab.app.entity.UserEntity;
+import com.edu.ulab.app.mapper.UserMapper;
 import com.edu.ulab.app.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,27 +11,32 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserDao<UserEntity, Long> userDao;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserDao<UserEntity, Long> userDao, UserMapper userMapper) {
+        this.userDao = userDao;
+        this.userMapper = userMapper;
+    }
+
     @Override
     public UserDto createUser(UserDto userDto) {
-        // сгенерировать идентификатор
-        // создать пользователя
-        // вернуть сохраненного пользователя со всеми необходимыми полями id
-        userDto.setId(1L);
-        return userDto;
+        return userMapper.userEntityToUserDto(userDao.createUser(userMapper.userDtoToUserEntity(userDto)));
     }
 
     @Override
     public UserDto updateUser(UserDto userDto) {
-        return null;
+        return userMapper.userEntityToUserDto(userDao.updateUser(userMapper.userDtoToUserEntity(userDto)));
     }
 
     @Override
     public UserDto getUserById(Long id) {
-        return null;
+        return userMapper.userEntityToUserDto(userDao.getUserById(id));
     }
 
     @Override
     public void deleteUserById(Long id) {
-
+        userDao.deleteUserById(id);
     }
 }
